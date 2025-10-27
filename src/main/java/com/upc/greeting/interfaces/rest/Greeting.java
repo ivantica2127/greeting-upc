2a2c49e30a38d0c96583bec5fdd2c0d43e719140
ivantica2127 <ivanprogramando@gmail.com>
2025-10-26 23:30:50 -0500

    Reorganiza Greeting.java dentro de interfaces/rest

--- src/main/java/com/upc/greeting/interfaces/rest/Greeting.java
+++ src/main/java/com/upc/greeting/interfaces/rest/Greeting.java
@@ -1,8 +1,9 @@
-package com.upc.greeting.interfaces;
+package com.upc.greeting.interfaces.rest;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Greeting {
 
     @GetMapping
-    public String greeting() {
-        return "Greetings from Spring Boot!";
+    public String greeting(@RequestParam(defaultValue = "world") String nombre) {
+        return "Greetings from Spring Boot!"+nombre;
     }
 }
